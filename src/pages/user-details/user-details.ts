a2f69bc64a71ff1f1d4448d68938b5fe469abb38
Javier Ruiz Vázquez <java.rv87@gmail.com>
2017-01-02 12:51:07 -0600

    Se agrega funcion para obtener los datos del usuario

--- src/pages/user-details/user-details.ts
+++ src/pages/user-details/user-details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { User } from '../../models/user';
+import { GithubUsers } from '../../providers/github-users';
 /*
   Generated class for the UserDetails page.
 
@@ -12,10 +13,14 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user-details.html'
 })
 export class UserDetailsPage {
+  user: User;
   login: string;
-
-  constructor(public navCtrl: NavController, private navParams: NavParams) {
+  constructor(public navCtrl: NavController, private navParams: NavParams, private githubUsers: GithubUsers) {
     this.login = navParams.get('login');
+    githubUsers.loadDetails(this.login).subscribe(user => {
+      this.user = user;
+      console.log(user)
+    })
   }
 
   ionViewDidLoad() {
--- src/providers/github-users.ts
+++ src/providers/github-users.ts
@@ -13,10 +13,17 @@ import { User } from '../models/user';
 @Injectable()
 export class GithubUsers {
   githubApiUrl = 'https://api.github.com';
+
   constructor(public http: Http) {
     console.log('Hello GithubUsers Provider');
   }
+
   load(): Observable<User[]> {
     return this.http.get(`${this.githubApiUrl}/users`).map(res => <User[]>res.json());
+  }
+
+  loadDetails(login: string): Observable<User> {
+    return this.http.get(`${this.githubApiUrl}/users/${login}`)
+      .map(res => <User>(res.json()))
   }
 }
